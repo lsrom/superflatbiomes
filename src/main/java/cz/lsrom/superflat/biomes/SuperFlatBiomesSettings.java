84f554206024da509adaa4e0b668004f46ab65a3
lsrom <lukas.srom@gmail.com>
2022-10-05 12:45:10 +0200

    Remove ancient city config as they are probably Jigsaw structure type

--- src/main/java/cz/lsrom/superflat/biomes/SuperFlatBiomesSettings.java
+++ src/main/java/cz/lsrom/superflat/biomes/SuperFlatBiomesSettings.java
@@ -23,8 +23,5 @@ public class SuperFlatBiomesSettings {
     @Rule(categories = GENERATION)
     public static boolean generateStrongholds = true;
 
-    @Rule(categories = GENERATION)
-    public static boolean generateAncientCities = true;
-
     public static List<Block> worldBlocks = Arrays.asList(Blocks.BEDROCK, Blocks.STONE, Blocks.STONE, Blocks.GRASS_BLOCK);
 }
