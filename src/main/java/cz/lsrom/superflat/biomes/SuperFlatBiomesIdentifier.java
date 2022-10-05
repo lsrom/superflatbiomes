5f77a033fedd1ee957c89f3dab7d649482ffbb5b
lsrom <lukas.srom@gmail.com>
2022-10-05 12:35:00 +0200

    Add identifier class

---
+++ src/main/java/cz/lsrom/superflat/biomes/SuperFlatBiomesIdentifier.java
@@ -0,0 +1,9 @@
+package cz.lsrom.superflat.biomes;
+
+import net.minecraft.util.Identifier;
+
+public class SuperFlatBiomesIdentifier extends Identifier {
+    public SuperFlatBiomesIdentifier(String path) {
+        super("superflatbiomes", path);
+    }
+}
